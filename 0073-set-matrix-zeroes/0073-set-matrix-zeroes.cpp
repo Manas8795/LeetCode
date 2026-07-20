4cbfe4f2a40d6e6dbf60ac6d7e14ee67d27d5bc4
Manas Agrawal ( Tempest ) <90537654+Manas8795@users.noreply.github.com>
2026-07-20 13:12:48 +0530

    Time: 0 ms (100%), Space: 20.8 MB (77.2%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -3,35 +3,25 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int m = matrix.size();
         int n = matrix[0].size();
-        // cout<<n;
-        // cout<<m;
-        vector<int> i1;
-        vector<int> j1;
-        for(int i = 0;i<m;i++)
-        {
-            for(int j = 0;j<n;j++)
-            {
-                if(matrix[i][j]==0)
-                {
-                    i1.push_back(i);
-                    j1.push_back(j);
+        
+        vector<bool> zeroRows(m, false);
+        vector<bool> zeroCols(n, false); 
+    
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(matrix[i][j] == 0) {
+                    zeroRows[i] = true;
+                    zeroCols[j] = true;
                 }
             }
         }
-        for(int i = 0;i<i1.size();i++)
-        {
-            for(int j = 0;j<n;j++)
-            {
-                matrix[i1[i]][j]=0;
-            }
-        }
-        for(int j = 0;j<j1.size();j++)
-        {
-            for(int i = 0;i<m;i++)
-            {
-                matrix[i][j1[j]]=0;
-            }
-        }
         
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(zeroRows[i] || zeroCols[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
     }
 };
