53bafc866b3feb0a1c958ee000b1d9d6fbcb0231
Manas Agrawal ( Tempest ) <90537654+Manas8795@users.noreply.github.com>
2026-08-13 21:05:38 +0530

    Time: 86 ms (16.3%), Space: 146.5 MB (26.76%) - LeetHub

---
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+       unordered_map<int,int> remainder; 
+       remainder[0] = -1;
+       int sum = 0;   
+       for(int i = 0; i<nums.size();i++) 
+       {
+            sum += nums[i];
+            int rem = sum%k;
+            if(remainder.count(rem)>0)
+            {
+                if(i - remainder[rem] >=2)
+                {
+                    return true;
+                }
+                
+            }
+            else
+            {
+                remainder[rem] = i;
+            }         
+       }
+       return false;
+    }
+};
