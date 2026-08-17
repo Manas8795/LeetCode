6d13b6ad5badd9bfb798de686c6dac45b7b4277b
Manas Agrawal ( Tempest ) <90537654+Manas8795@users.noreply.github.com>
2026-08-17 18:17:21 +0530

    Time: 9 ms (54.46%), Space: 11.7 MB (81.63%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;          // Move 1 step
+            fast = fast->next->next;    // Move 2 steps
+
+            if (slow == fast) return true; // Cycle detected!
+        }
+
+        return false; // Fast reached NULL -> No cycle
+    }
+};
