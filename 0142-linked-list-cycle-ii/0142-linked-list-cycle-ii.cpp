94019326023d43452e2b5849b430dce477d0ec73
Manas Agrawal ( Tempest ) <90537654+Manas8795@users.noreply.github.com>
2026-08-17 18:39:00 +0530

    Time: 16 ms (5.18%), Space: 13.9 MB (6.88%) - LeetHub

---
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* detectCycle(ListNode *head) {
+        unordered_map<ListNode*,int> m;
+        ListNode* temp = head;
+        int x = 1;
+        if(head == NULL) return temp;
+        
+        while(temp!=NULL)
+        {
+            if(m.count(temp)>0) return temp;
+            else
+            {
+                m[temp] == x;
+                x++;
+            }
+            temp = temp->next;
+        }
+        return temp;
+    }
+};
