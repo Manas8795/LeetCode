e6437ed972a00dc7280331285616b885c3d17ae5
Manas Agrawal ( Tempest ) <90537654+Manas8795@users.noreply.github.com>
2026-08-24 14:51:58 +0530

    Time: 3 ms (89.67%), Space: 15.3 MB (44.17%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.cpp
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.cpp
@@ -1,94 +1,46 @@
 class Solution {
 public:
-    int calculate(string sf) {
-        string s="";
-        for(char i : sf)
+    int calculate(string s) {
+        stack<int>st;
+        int num=0;
+        char op='+';
+        for(int i=0;i<s.length();i++)
         {
-            if(i != ' ')
+    
+          
+
+            if(s[i]>='0' && s[i]<='9')
             {
-                s += i;
+                num=num*10+(s[i]-'0');
             }
-        }
-        vector<string> st;
-        int j = 0;
-        unordered_map<char,int> f;
-        f['+'] = 1;
-        f['-'] = 1;
-        f['/'] = 1;
-        f['*'] = 1;
-        for(int i = 0;i<s.size();i++)
-        {
-            if(f[s[i]]>0) 
+             if((s[i]!=' ' && !(s[i]>='0' && s[i]<='9')) || i==s.length()-1)
             {
-                st.push_back(s.substr(j,i-j));
-                st.push_back(string(1,s[i]));
-                j = i+1;
-            }
-        }
-        st.push_back(s.substr(j,s.size()-j));
-        unordered_map<string,int> op;
-        op["+"] = 1;
-        op["-"] = 1;
-        op["/"] = 2;
-        op["*"] = 2;
-        stack<int> operand;
-        stack<string> operat;
-        for(string x : st)
-        {
-            if(op[x]>0)
-            {
-                if(operat.empty())
+                if(op=='+')
+                st.push(num);
+                else if(op=='-')
+                st.push(-num);
+                else if(op=='*')
                 {
-                    operat.push(x);
-                }
-                else if(op[x]<=op[operat.top()])
-                {
-                    while (!operat.empty() && op[operat.top()] >= op[x])
-                    {
-                        int b = operand.top();operand.pop();
-                        int a = operand.top();operand.pop();
-                        string y = operat.top();
-                        int z;
-                        if(y=="+") z = a+b;
-                        if(y=="-") z = a-b;
-                        if(y=="/") z = a/b;
-                        if(y=="*") z = a*b;
-                        operand.push(z);
-                        operat.pop();                            
-                    }
-                    operat.push(x);
+                    int n=st.top();
+                    st.pop();
+                    st.push(n*num);
                 }
                 else
                 {
-                    operat.push(x);
+                    int n=st.top();
+                    st.pop();
+                    st.push(n/num);
                 }
-            }
-            else
-            {
-                operand.push(stoi(x));
+                num=0;
+                op=s[i];
             }
         }
-        while (!operat.empty()) {
-
-            int b = operand.top();
-            operand.pop();
-
-            int a = operand.top();
-            operand.pop();
-
-            string y = operat.top();
-            operat.pop();
-
-            int z;
-
-            if (y == "+") z = a + b;
-            if (y == "-") z = a - b;
-            if (y == "/") z = a / b;
-            if (y == "*") z = a * b;
-
-            operand.push(z);
+        int sum=0;
+        while(!st.empty())
+        {
+            sum+=st.top();
+            st.pop();
         }
-        int a = operand.top();operand.pop();
-        return a;
+        return sum;
     }
 };
