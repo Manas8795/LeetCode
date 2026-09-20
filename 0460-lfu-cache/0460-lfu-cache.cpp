2a0d367a188debf397e5a6360201f68fadbddff9
Manas Agrawal ( Tempest ) <90537654+Manas8795@users.noreply.github.com>
2026-09-20 13:28:20 +0530

    Time: 103 ms (64.23%), Space: 194.8 MB (29.65%) - LeetHub

--- 0460-lfu-cache/0460-lfu-cache.cpp
+++ 0460-lfu-cache/0460-lfu-cache.cpp
@@ -1,34 +1,38 @@
 class LFUCache {
 public:
-    unordered_map<int,pair<int,int>> k;
-    unordered_map<int,vector<int>> freq;
+    unordered_map<int, pair<int, int>> k;
+    unordered_map<int, list<int>> freq;
+    unordered_map<int, list<int>::iterator> iter;
     int mini = 1;
     int capacity;
+
     LFUCache(int capacity) {
         this->capacity = capacity;
     }
     
     int get(int key) {
-        if(k.find(key)!=k.end()) 
+        if(k.find(key) != k.end()) 
         {
             int a = k[key].second++;
             int b = a + 1;
             
-            for(int i = 0;i<freq[a].size();i++)
-            {
-                if(freq[a][i] == key) freq[a].erase(freq[a].begin() + i);
-            }
+            freq[a].erase(iter[key]);
+            
             if(freq[a].empty() && mini == a) mini++;
-            freq[b].push_back(key);
+            
+            freq[b].push_front(key);
+            iter[key] = freq[b].begin();
+            
             return k[key].first;
         }
         return -1;
     }
     
     void put(int key, int value) {
-        if(k.find(key)!=k.end())
-        {
+        if (capacity <= 0) return;
 
+        if(k.find(key) != k.end())
+        {
             get(key);
             k[key].first = value;
         }
@@ -36,22 +40,23 @@ public:
         {
             if(k.size() == capacity)
             {
-                int a = freq[mini][0];
-                freq[mini].erase(freq[mini].begin());
-                k.erase(a);
-
+                int evictKey = freq[mini].back();
+                freq[mini].pop_back();
+                
+                if(freq[mini].empty()) {
+                    freq.erase(mini);
+                }
+                
+                k.erase(evictKey);
+                iter.erase(evictKey);
             }
+            
             k[key].first = value;
             k[key].second = 1;
-            freq[1].push_back(key);
+            
+            freq[1].push_front(key);
+            iter[key] = freq[1].begin();
             mini = 1;
         }
     }
 };
-
-/**
- * Your LFUCache object will be instantiated and called as such:
- * LFUCache* obj = new LFUCache(capacity);
- * int param_1 = obj->get(key);
- * obj->put(key,value);
- */
